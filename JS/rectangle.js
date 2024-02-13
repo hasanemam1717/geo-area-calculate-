c266eda9d17ce5baffbb7b0367854737f3dff57e
hasanemam1717 <hasanemam1717@gmail.com>
2024-02-13 11:38:36 +0600

    added a ractangle calculate function

--- JS/rectangle.js
+++ JS/rectangle.js
@@ -1,3 +1,19 @@
-function calculateRactangleArea (){
-    console.log('cliced')
+function calculateRactangleArea(){
+    // ractangle width calculation
+    const ractangleWidth =document.getElementById('rectangleWidth');
+    const rectangleWidthValue =ractangleWidth.value;
+    const width =parseFloat(rectangleWidthValue)
+    // console.log(typeof width)
+
+    // ractangle length calculate
+    const ractangleLength =document.getElementById('rectangleLength');
+    const rectangleLengthValue =ractangleLength.value;
+    const length =parseFloat(rectangleLengthValue)
+
+    // ractangle area 
+    const area = width * length
+    // console.log(area)
+    const rectangleAreaDisplay =document.getElementById('rectangleArea');
+    rectangleAreaDisplay.innerText=area;
+
 }
